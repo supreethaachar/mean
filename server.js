91d3e790a696450136eba7ef32ab97c15398b2db
Supreetha  Achar K <sachar5@asiapac.globalcsc.net>
2018-10-30 20:14:24 +0530

    changes in server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8000);
 
 
 app.get("/*", function(req, res) {
